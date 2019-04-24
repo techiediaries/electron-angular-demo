c73d2c5cfdd8297721386290cd0a0092c36fb402
Ahmed <techiediaries9@gmail.com>
2019-04-24 18:21:26 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+const {app, BrowserWindow, ipcMain} = require('electron')
+const url = require("url");
+const path = require("path");
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/dist/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
+
+function openModal(){
+  const { BrowserWindow } = require('electron');
+  let modal = new BrowserWindow({ parent: mainWindow, modal: true, show: false })
+  modal.loadURL('https://www.sitepoint.com')
+  modal.once('ready-to-show', () => {
+    modal.show()
+  })
+}
+
+ipcMain.on('openModal', (event, arg) => {
+  openModal()
+})
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IpcRenderer } from 'electron';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'electron-angular-demo';
+  private ipc: IpcRenderer
+
+  constructor(){
+    if ((<any>window).require) {
+      try {
+        this.ipc = (<any>window).require('electron').ipcRenderer;
+      } catch (e) {
+        throw e;
+      }
+
+    } else {
+      console.warn('App not running inside Electron!');
+    }
+  }
+  
+  openModal(){
+    console.log("Open a modal");
+    this.ipc.send("openModal");
+  }
 }
